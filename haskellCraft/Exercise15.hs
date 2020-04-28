e35f40af04d45cda9be2155d47588aace29f0ae6
qtEliyahu <mr.gorden.smit@gmail.com>
2020-04-28 17:29:09 +0300

    ** New module ** added few exersices

---
+++ haskellCraft/Exercise15.hs
@@ -0,0 +1,7 @@
+-- 15.1 skipped
+
+-- 15.2
+-- Explain why you think is is the default that imported definitions are not themselves exported.
+-- I think it's because of code's safety.
+
+-- 15.3 skipped
