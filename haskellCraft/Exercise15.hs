0dd5b5593807077535ba60af7c5a029a319afe47
qtEliyahu <mr.gorden.smit@gmail.com>
2020-04-29 15:44:21 +0300

    added few exercises

--- haskellCraft/Exercise15.hs
+++ haskellCraft/Exercise15.hs
@@ -5,3 +5,28 @@
 -- I think it's because of code's safety.
 
 -- 15.3 skipped
+
+-- Exercise 15.4
+-- What is the coding of the message battat using the following tree?
+{-
+ /\
+b /\
+ a  t
+-}
+
+-- L RL RR RR RL RR
+-- lenght is 11 while the others is 10 and 9
+
+-- Exercise 15.5
+-- Using the first coding tree decode the coded message
+-- RL L RL RL L RR
+{-
+ /\
+a /\
+ b  t
+-}
+
+-- babbat
+
+-- Which tree would you expect to give the best coding of the message?
+-- Tree from exercise 15.3 because in the message b is domiating and the tree has one length "bit" for b
