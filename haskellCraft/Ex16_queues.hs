9aa8f9cbbb584d799edd1ef7c847ac4bceeb7b02
qtEliyahu <mr.gorden.smit@gmail.com>
2020-05-06 18:08:23 +0300

    smoll reforge, added exercise

--- haskellCraft/Ex16_queues.hs
+++ haskellCraft/Ex16_queues.hs
@@ -1,4 +1,10 @@
-module Queue where
+module Queue
+( Queue,
+  emptyQ,   -- Queue a
+  isEmptyQ, -- Queue a -> Bool
+  addQ,     -- a -> Queue a -> Queue a
+  remQ      -- Queue a -> (a, Queue a)
+  ) where
 
 newtype Queue a = Qu [a]
 
@@ -35,4 +41,46 @@ addQ x (Qu xs ys) = Qu xs (x:ys)
 remQ (Qu (x:xs) ys) = (x, Qu xs ys)
 remQ (Qu [] (y:ys)) = remQ (Qu (reverse (y:ys)) [])
 remQ (Qu [] []) = error "remQ"
+-}
+
+-- Ex 16.7
+-- Give calcutation of
+{-
+"abcde" ++ "f"
+
+[] ++ ys - False
+(x:xs) ys - True
+  a : "bcde" ++ "f"
+  ...
+    a : b : c : d : e : ([] "f" - True)
+    "f"
+  [] ++ ys - True
+  a : b : c : d : e : "f"
+  ...
+"abcdef"
+
+init "abcdef"
+init x = take (length - 1) x
+
+take (length "abcdef" - 1) "abcdef"
+      length "abcdef" = 1 + length "bcdef" + ... = 6
+      6 - 1 = 5
+take 5 "abcdef"
+a : take 4 "bcdef"
+...
+a : b : c : d : take 0 "ef"
+a : b : c : d : []
+"abcd"
+
+last "abcdef"
+last x = x !! (length x-1)
+
+"abcdef" !! (length "abcdef" - 1)
+"abcdef" !! (6 - 1)
+"abcdef" !! 5
+(!!) "abcdef" 5
+     a : "bcdef" 4
+     ...
+     a : b : c : d : e : "f" 0
+     'f'
 -}
