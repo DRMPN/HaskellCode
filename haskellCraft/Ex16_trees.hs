07fed8ab4648ff42166bcab2d6c0259a479cb7ac
qtEliyahu <mr.gorden.smit@gmail.com>
2020-05-15 19:49:04 +0300

    added few definitions

--- haskellCraft/Ex16_trees.hs
+++ haskellCraft/Ex16_trees.hs
@@ -1,12 +1,40 @@
 module Tree
   ( Tree,
-    nil,
-    isNil,
-    isNode,
-    leftSub,
-    rightSub,
-    treeVal,
-    insTree,
-    delete,
-    minTree)
+    nil,      -- Tree a
+    isNil,    -- Tree a -> Bool
+    isNode,   -- Tree a -> Bool
+    leftSub,  -- Tree a -> Tree a
+    rightSub, -- Tree a -> Tree a
+    treeVal,  -- Tree a -> a
+    insTree,  -- Ord a => a -> Tree a -> Tree a
+    delete,   -- Ord a => a -> Tree a -> Tree a
+    minTree)  -- Ord a => Tree a -> Maybe a
   where
+
+data Tree a = Nil | Node a (Tree a) (Tree a)
+
+nil :: Tree a
+nil = Nil
+
+
+isNil, isNode :: Tree a -> Bool
+
+isNil Nil = True
+isNil _ = False
+
+isNode (Node _ _ _) = True
+isNode _ = False
+
+
+leftSub, rightSub :: Tree a -> Tree a
+
+leftSub Nil = error "No leftSub"
+leftSub (Node _ t1 _) = t1
+
+rightSub Nil = error "No rightSub"
+rightSub (Node _ _ t2) = t2
+
+
+treeVal :: Tree a -> a
+treeVal Nil = error "No value"
+treeVal (Node v _ _) = v
