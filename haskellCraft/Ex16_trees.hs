a5eeab6c228f9014c10bf9af1a08c2c5ce94b225
qtEliyahu <mr.gorden.smit@gmail.com>
2020-05-16 18:13:46 +0300

    New functions has been added

--- haskellCraft/Ex16_trees.hs
+++ haskellCraft/Ex16_trees.hs
@@ -38,3 +38,18 @@ rightSub (Node _ _ t2) = t2
 treeVal :: Tree a -> a
 treeVal Nil = error "No value"
 treeVal (Node v _ _) = v
+
+insTree :: Ord a => a -> Tree a -> Tree a
+insTree val Nil = Nil
+insTree val (Node v t1 t2)
+  | val == v = Node v t1 t2
+  | val > v = Node v t1 (insTree val t2)
+  | val < v = Node v (insTree val t1) t2
+
+delete :: Ord a => a -> Tree a -> Tree a
+delete val (Node v t1 t2)
+  | val < v = Node v (delete val t1) t2
+  | val > v = Node v t1 (delete val t2)
+  | isNil t2 = t1
+  | isNil t1 = t2
+  | otherwise = join t1 t2
